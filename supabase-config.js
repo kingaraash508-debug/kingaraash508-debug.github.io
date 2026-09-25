0cde13b287dc64cd770e5d4553263060c5757b26
kingaraash508-debug <kingaraash508@gmail.com>
2026-09-25 13:38:42 +0300

    Use Supabase anon key for browser API

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.MTAAFIX_SUPABASE = {
     url: 'https://oisbdqaovccvtcbyazap.supabase.co',
-    anonKey: 'sb_publishable_ltaNA7nnVozoSCOcZIjg'
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pc2JkcWFvdmNjdnRjYnlhemFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzMjg3OTAsImV4cCI6MjEwNTkwNDc5MH0.CQgQ4qzb9z-G-6pvehZG9kd8fR2qUR8I0oHgg_jX6YU'
 };
